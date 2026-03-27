9ebc1cfc92e0e6b397410a172b3399a8c2caefd8
Mateo-UIDE <macharroja@uide.edu.ec>
2026-03-27 14:46:12 +0000

    wip:proc directory exploration

--- filesystem_script.sh
+++ filesystem_script.sh
@@ -1,5 +1,10 @@
 #change directory to dev directory
 cd /dev
-
 #list archivies and directories
 ls
+#change directory to proc directory
+cd /proc
+#list process on proc directory
+ls
+#shows cpu information
+cat cpuinfo
