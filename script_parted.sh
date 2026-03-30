33f40c1a9ac80c533afd40cd431f9bc0dd24453a
Mateo-UIDE <macharroja@uide.edu.ec>
2026-03-30 15:53:03 +0000

    stat

--- script_parted.sh
+++ script_parted.sh
@@ -14,3 +14,16 @@ sudo parted -l
 lsblk -f
 #Check if the folder exists; if it does, identify whether the system booted in UEFI or BIOS.
 [ -d /sys7firmware/efi ] && echo "UEFI" || echo "BIOS"
+#Create a file called test.txt and write the text "myfile" inside it.
+echo "miarchivo" >test.txt
+#uestra información detallada sobre el archivo test.txt
+stat test.txt
+
+File: test.txt
+  Size: 10              Blocks: 8          IO Block: 4096   regular file
+Device: 7,4     Inode: 1572909     Links: 1
+Access: (0666/-rw-rw-rw-)  Uid: ( 1000/codespace)   Gid: ( 1000/codespace)
+Access: 2026-03-30 15:49:02.012355761 +0000
+Modify: 2026-03-30 15:49:02.013355761 +0000
+Change: 2026-03-30 15:49:02.013355761 +0000
+ Birth: 2026-03-30 15:49:02.012355761 +0000
