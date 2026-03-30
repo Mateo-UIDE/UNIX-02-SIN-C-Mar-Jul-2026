1f0dd0989fc3394cf8111c847d75edaa6db7d68a
Mateo-UIDE <macharroja@uide.edu.ec>
2026-03-30 15:27:11 +0000

    disk and partition

--- script_parted.sh
+++ script_parted.sh
@@ -4,3 +4,11 @@ sudo apt update
 sudo apt upgrade
 #install the parted program on the system
 sudo apt install parted
+#install the numpy library in Python
+pip install numpy
+#execute several commands in succession to display disk and partition information
+sudo parted -l && echo -e "\n--\n" && lsblk -f && e "\n--\n"
+#show information of all disks and partitions
+sudo parted -l
+#sample the disks and partitions of the system with his archives system
+lsblk -f
