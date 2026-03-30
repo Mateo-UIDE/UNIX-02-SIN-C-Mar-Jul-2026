c110892661f5c3559d79c0da739a9a578e6aeb7c
Mateo-UIDE <macharroja@uide.edu.ec>
2026-03-30 15:08:31 +0000

    install parted

---
+++ script_parted.sh
@@ -0,0 +1,6 @@
+#Update the URLs where we have the libraries
+sudo apt update 
+#updates the systems installed on the system
+sudo apt upgrade
+#install the parted program on the system
+sudo apt install parted
