f92a2aa168888f1c40782ff12c5e9c8fd08997f1
Mateo-UIDE <macharroja@uide.edu.ec>
2026-04-08 21:19:26 +0000

    wip:same options

--- script_boot.sh
+++ script_boot.sh
@@ -1,4 +1,9 @@
 cd clase
-#ruta relativa
+#relative route
 /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/clase
-#ruta absoluta
+#absolute route
+ls -lai
+#shows the inodo
+#l-list a=all i-inode
+ls -l -a -i
+#the same options but in a separate way
