aaf1e8e8d844956fe5901d67f3c22568eb12b1ea
Mateo-UIDE <macharroja@uide.edu.ec>
2026-04-20 14:45:47 +0000

    wip:sh

--- boot_exploration.sh
+++ boot_exploration.sh
@@ -57,3 +57,5 @@ sudo echo "hola" >/etc/archivo_protegido
 echo "hola" | sudo tee /etc/archivo_protegido>/dev/null
 #pipes the string "hola" into the tee utility, which is executed with root privileges. tee writes the input to the specified protected file and simultaneously displays the output in the terminal.
 echo "hola" | sudo tee /etc/archivo_protegido
+#invokes a new shell (sh) with administrative privileges to execute a specific string of commands. This allows the shell itself to handle the output redirection (>>), enabling the user to append text to a protected system file.
+sudo sh -c 'echo "chao" >> /etc/archivo_protegido'
