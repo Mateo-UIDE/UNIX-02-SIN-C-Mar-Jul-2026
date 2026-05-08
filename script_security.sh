0aad4badd6edd221ec98aa0ed0708e6707a3d977
Mateo-UIDE <macharroja@uide.edu.ec>
2026-05-08 14:44:17 +0000

    wip:security

---
+++ script_security.sh
@@ -0,0 +1,8 @@
+#Prints the UID (User ID), GID (Group ID), and the Groups the current user belongs to.
+id
+#Lists all the security groups associated with the current user session.
+groups
+#Displays the content of the system file that contains basic information for every user account on the Linux system.
+cat /etc/passwd
+#Reads the user account file and displays only the first 10 lines.
+cat /etc/passwd | head -10
