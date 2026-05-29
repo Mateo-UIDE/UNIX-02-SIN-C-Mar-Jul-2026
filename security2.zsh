475b5c93d231b2e48272e1793bcfe507f3e13808
Mateo-UIDE <macharroja@uide.edu.ec>
2026-05-29 15:00:22 +0000

    wip:id

---
+++ security2.zsh
@@ -0,0 +1,8 @@
+#Displays the current user's ID and group information.
+id
+#Shows the name of the current user's primary group.
+id -gn
+#Creates an empty file named test_grupo_heredado.txt in the home directory.
+touch ~/test_grupo_heredado.txt
+#Displays detailed information about the file, including permissions and owner.
+ls -la ~/test_grupo_heredado.txt
