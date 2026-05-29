644783b2288bf3fe1822b97ec0dc1b49b64ffd63
Mateo-UIDE <macharroja@uide.edu.ec>
2026-05-29 15:47:19 +0000

    wip:mkdir

--- security2.zsh
+++ security2.zsh
@@ -28,3 +28,10 @@ echo $$
 #Closes the current shell session.
 exit
 echo $$
+#
+touch ~/dentro_de_newgrp.txt   
+ls -la ~/dentro_de_newgrp.txt
+#Creates the directory path proyecto_dev/src, including parent folders if needed.
+mkdir -p~/proyecto_dev/src
+#Lists files in the home directory with detailed information and file sizes.
+ls -ls ~/
