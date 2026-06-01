5f1f1f9afa0f27d416c21cc453a16b1501a52f95
Mateo-UIDE <macharroja@uide.edu.ec>
2026-06-01 15:27:27 +0000

    wip:basic_syntax

---
+++ blackhatbash2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# All this script does is create a directory, create a file
+# within the directory, and then list the contents of the di
+rectory.
+mkdir mydirectory
+touch mydirectory/myfile
+ls -l mydirectory
+Listing 1-9: A bash script that
