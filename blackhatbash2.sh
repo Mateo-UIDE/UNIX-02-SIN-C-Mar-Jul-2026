a3553ec47476b6edd4c5e9db0c8b1a65987fa6c8
Mateo-UIDE <macharroja@uide.edu.ec>
2026-06-01 15:29:51 +0000

    wip:basic_syntax_2

--- blackhatbash2.sh
+++ blackhatbash2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # All this script does is create a directory, create a file
-# within the directory, and then list the contents of the di
-rectory.
+# within the directory, and then list the contents of the directory.
 mkdir mydirectory
+set -x
 touch mydirectory/myfile
 ls -l mydirectory
-Listing 1-9: A bash script that
+set +x
