07bcbd2591b1eff7a9321124a899d81fb54e7c5e
Mateo-UIDE <macharroja@uide.edu.ec>
2026-06-05 15:16:15 +0000

    wip:control_operators2

--- background.sh
+++ background.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # This script will send the sleep command to the background.
-echo "Sleeping for 10 seconds..." ❶ sleep 10 &
+echo "Sleeping for 10 seconds..." 
+sleep 10 &
 # Creates a file
 echo "Creating the file test123"
 touch test123
