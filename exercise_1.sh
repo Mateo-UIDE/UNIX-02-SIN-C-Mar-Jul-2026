9431e53a5d454c021769e27fcb059ce68852f9b2
Mateo-UIDE <macharroja@uide.edu.ec>
2026-06-09 02:01:22 +0000

    wip:exercise1-1

---
+++ exercise_1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# 1. Accepts two arguments on the command line and assigns them to
+#variables. The first argument should be your first name, and the second
+#should be your last name.
+# $1 stores the first argument and $2 stores the second argument received
+PRIMER_NOMBRE=$1
+APELLIDO=$2
