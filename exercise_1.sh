4a574e0a268ceae2ce3afd2bf31e443668beb1c8
Mateo-UIDE <macharroja@uide.edu.ec>
2026-06-09 02:03:54 +0000

    wip:exercise1-2/3

--- exercise_1.sh
+++ exercise_1.sh
@@ -5,3 +5,8 @@
 # $1 stores the first argument and $2 stores the second argument received
 PRIMER_NOMBRE=$1
 APELLIDO=$2
+
+# 2 and 3. Create the output.txt file and save the current date
+# The format %d-%m-%Y complies with the bonus (DD-MM-YYYY)
+# We use a single '>' to create the file (or overwrite it if it already exists).
+date +"%d-%m-%Y" > output.txt
